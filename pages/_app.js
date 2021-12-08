491d72bfa194e89f355cb34c62b609321168bab0
Stan <stanhoekema@gmail.com>
2021-12-08 15:21:44 +0100

    Google Analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 
 import '../styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <PlasmicRootProvider>
+      <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="GA" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
       <Component {...pageProps} />
+    </>
     </PlasmicRootProvider>
   );
 }
